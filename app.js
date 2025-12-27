765e7bc10cbacfee084405c510b3d15f308ef870
Hassanullah Usmani <hassanullahusmani45@gmail.com>
2025-12-27 15:56:12 +0430

    defult midellwares

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from "express";
 import { authRouter } from "./src/routes/auth.js";
-
+import cros from "cors";
+import bodyParser from "body-parser";
 export const app = express();
+
+// defult midellwares.
+app.use(cros());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 
 app.use("/auth", authRouter);
