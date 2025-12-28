579ae77cb7e3f69e5e44fa6a200bf4fcf45df2a1
Hassanullah Usmani <hassanullahusmani45@gmail.com>
2025-12-28 21:47:03 +0430

    update user coding and logic.

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,6 +2,7 @@ import { isValidObjectId } from "mongoose";
 import { banuserModel } from "../models/banuser.js";
 import { userModel } from "../models/users.js";
 import { validateBanuser } from "../validators/banuser.js";
+import bcrypt from "bcrypt";
 
 export const banUser = async (req, res) => {
     const { phone } = req.body;
@@ -51,7 +52,7 @@ export const deleteUser = async (req, res) => {
         }
 
         const user = await userModel.deleteOne({ _id: id });
-        if (user.deletedCount>0) {
+        if (user.deletedCount > 0) {
             return res.status(200).json({
                 message: "User is deleted successfuly."
             });
@@ -63,5 +64,36 @@ export const deleteUser = async (req, res) => {
         res.status(400).json({
             message: error
         });
+    }
+}
+
+
+
+export const updateUser = async (req, res) => {
+    try {
+        const user = req.authUser;
+        const {
+            name = user.name,
+            username = user.username,
+            email = user.email,
+            password,
+            phone = user.phone
+        } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const updatedUser = await userModel.
+            findByIdAndUpdate({ _id: user.id }, {
+                name,
+                username,
+                email,
+                phone,
+                password: hashedPassword
+            }).select("-password -__v");
+
+        res.status(200).send({
+            message: "Yor info is updated successfuly.",
+            updatedUser
+        });
+    } catch (error) {
+        res.status(400).send({ error });
     }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { banUser, deleteUser, getUsers } from "../controllers/user.js";
+import { banUser, deleteUser, getUsers, updateUser } from "../controllers/user.js";
 import { auth } from "../middlewares/auth.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
 
@@ -10,3 +10,4 @@ export const userRouter = express.Router();
 userRouter.post("/ban", auth, isAdmin, banUser);
 userRouter.get("/", auth, getUsers);
 userRouter.delete("/", auth, deleteUser);
+userRouter.put("/", auth, updateUser);
